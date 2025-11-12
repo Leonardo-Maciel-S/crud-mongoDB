6a9e07af9ba90908471a06ed29658e2fc3f9384b
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-12 13:37:43 -0300

    feat: create user service

---
+++ src/services/user/create.ts
@@ -0,0 +1,22 @@
+import type { User } from "../../interfaces/user.js";
+import user from "../../model/user.js";
+
+export const create = async (body: User) => {
+  const userExist = await user.findOne({ email: body.email });
+
+  if (userExist) {
+    throw new Error("Usuário já existe");
+  }
+
+  try {
+    const userCreated = await user.create(body);
+
+    if (!userCreated) {
+      throw new Error("Erro ao criar usuário");
+    }
+
+    return userCreated;
+  } catch (error) {
+    throw new Error("Erro inesperado");
+  }
+};
